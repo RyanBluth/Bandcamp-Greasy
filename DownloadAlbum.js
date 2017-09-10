8b92d783c14ee9cf6f4716ea41e81fe264bdf779
RyanBluth <ryanbluth93@gmail.com>
2017-09-10 10:09:46 -0400

    Added format selection

--- DownloadAlbum.js
+++ DownloadAlbum.js
@@ -8,21 +8,42 @@
 // ==/UserScript==
 (function () {
     'use strict';
-    var interval = setInterval(function () {
-        var errorText = document.getElementsByClassName("error-text")[0];
-        if (errorText.offsetParent !== null) {
-            location.reload();
-        }
-        try {
-            var maintenanceLink = document.getElementsByTagName("a")[0];
-            if (a.href.indexOf("bandcampstatus") > 0) {
-                location.reload();
+    
+    // Set format here
+    var format = "MP3 320";
+    
+    var selectedFormat = false;
+
+    setTimeout(function(){    
+        var interval = setInterval(function () {
+            if(!selectedFormat){
+                document.getElementsByClassName('item-format button')[0].click();
+                var spans = document.getElementsByTagName("span");
+        
+                for (var i = 0; i < spans.length; i++) {
+                  if (spans[i].textContent == format) {
+                    spans[i].parentElement.click();
+                    selectedFormat = true;
+                    break;
+                  }
+                }
+            }else{
+                var errorText = document.getElementsByClassName("error-text")[0];
+                if (errorText.offsetParent !== null) {
+                    location.reload();
+                }
+                try {
+                    var maintenanceLink = document.getElementsByTagName("a")[0];
+                    if (a.href.indexOf("bandcampstatus") > 0) {
+                        location.reload();
+                    }
+                } catch (e) { }
+                var titleLabel = document.getElementsByClassName('download-title')[0];
+                if (titleLabel.children[0].href !== undefined && titleLabel.children[0].href.length > 0) {
+                    titleLabel.children[0].click();
+                    clearTimeout(interval);
+                }
             }
-        } catch (e) { }
-        var titleLabel = document.getElementsByClassName('download-title')[0];
-        if (titleLabel.children[0].href !== undefined && titleLabel.children[0].href.length > 0) {
-            titleLabel.children[0].click();
-            clearTimeout(interval);
-        }
+        }, 2000);
     }, 2000);
 })();
--- README.md
+++ README.md
@@ -4,5 +4,6 @@ A set of [Grease Monkey](https://addons.mozilla.org/en-US/firefox/addon/greasemo
 ## Usage
 1. Import both scripts into Grease Monkey or Tamper Monkey
 2. Change the value of YOUR_USERNAME to your actual username in DownloadCollection.js
+3. Change the value of format to your desired format(MP3 320 is the default) in DownloadAlbum.js
 3. Make sure Grease Monkey/Tamper Monkey is enabled.
 4. Navigate to your collection on Bandcamp and watch the magic happen
